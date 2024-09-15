ffb9df712f9f2f87f3936bc7e0af0fd275cb729f
Fuko <fuu.w2546@gmail.com>
2024-09-15 14:58:09 +0900

    コースリスト登録

--- src/main/java/Students/students/data/StudentsCourses.java
+++ src/main/java/Students/students/data/StudentsCourses.java
@@ -10,8 +10,19 @@ import lombok.Setter;
 public class StudentsCourses {
 
   private String id;
+
   private String studentId;
   private String courseName;
   private LocalDateTime courseStartAt;
   private LocalDateTime courseEndAt;
+
+  public StudentsCourses(String id, String studentId, String courseName,
+      LocalDateTime courseStartAt,
+      LocalDateTime courseEndAt) {
+    this.id = id;
+    this.studentId = studentId;
+    this.courseName = courseName;
+    this.courseStartAt = courseStartAt;
+    this.courseEndAt = courseEndAt;
+  }
 }
