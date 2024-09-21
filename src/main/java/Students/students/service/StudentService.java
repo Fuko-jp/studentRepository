ce32eddfdddeb58f73fb0862b0cc137b1e3fd743
Fuko <fuu.w2546@gmail.com>
2024-09-21 13:25:14 +0900

    registerStudentの実装

--- src/main/java/Students/students/service/StudentService.java
+++ src/main/java/Students/students/service/StudentService.java
@@ -2,10 +2,12 @@ package Students.students.service;
 
 import Students.students.data.Student;
 import Students.students.data.StudentsCourses;
+import Students.students.domain.StudentDetail;
 import Students.students.repository.StudentRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -20,14 +22,14 @@ public class StudentService {
     //検索処理
     repository.search();
 
-    //絞り込みをする。年齢が３０代の人のみを抽出する。
-    //抽出したリストをコントローラーに返す。
+
     return repository.search();
   }
   public List<StudentsCourses> searchStudentsCoursesList(){
-    //絞り込み検索で「javaコース」のコース情報のみを抽出する。
-    //抽出したリストをコントローラーに返す。
     return repository.sesrchStudentsCourses();
   }
-
+  @Transactional
+  public void registerStudent(StudentDetail studentDetail){
+    repository.registerStudent(studentDetail.getStudent());
+  }
 }
